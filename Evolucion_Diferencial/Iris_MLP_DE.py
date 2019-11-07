161faec55c75b41425a1a4c13b58814c2b061917
Luis Ronquillo <w580ix@gmail.com>
2019-11-07 13:49:26 -0600

    perceptron usando evolucion diferencial. en este punto solo falta las 35 corridas para obtener los estadisticos

--- Evolucion_Diferencial/Iris_MLP_DE.py
+++ Evolucion_Diferencial/Iris_MLP_DE.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
-#========================= Dataset iris plant
+#========================= Dataset iris plant =========================
 scaler = StandardScaler()
 '''Cargamos el conjunto de datos y lo dividimos en entradas y salidas'''
 database = 'iris plant'
@@ -19,46 +19,60 @@ iris_ds["variety"] = iris_ds["variety"].replace(["Virginica"], 2)
 data = iris_ds.columns.tolist()[:-1]
 target = iris_ds.columns.tolist()[-1]
 X = np.array(iris_ds[data])
+X_std = scaler.fit(X).transform(X) #dataset estandar
 y = np.array(iris_ds[target])
 y_bin = one_hot_encode(X_data=X, y_data=y, num_cols=3)
-X_std = scaler.fit(X).transform(X)
 #========================= Fin pre-procesamiento iris plant
 
-#========================= Constantes Perceptron con Evolución diferencial
+#========================= Constantes Perceptron Multicapa (Evolucion Diferencial) =========================
 Np = 30 #population
-Cr = 0.7 #crossover ratio
-F = 0.8 #mutacion
-Li = -10.0 #limite inferior
-Ls = 10.0 #limite superior
+Cr = 0.8 #crossover ratio
+F = 0.9 #mutacion
+Li = -100.0 #limite inferior
+Ls = 100.0 #limite superior
 generaciones = 1000 #Generaciones evolucion diferencial
+folders = 2
+aux = 0
+#========================= Fin constantes
 
-perceptron = MLP_DE(input_neurons=X.shape[1], output_neurons=y_bin.shape[1]) #inicializando objeto perceptron
-indices = perceptron.calcula_indices()
+#========================= Inicializando Objetos (clases) =========================
+perceptron = MLP_DE(input_neurons=X.shape[1], output_neurons=y_bin.shape[1]) #arquitectura 4 neuronas entrada y 3 salida
+indices = perceptron.calcula_indices() #inidices para calcular la dimensionalidad
 evolucion_diferencial = DE(Np=Np, Dim=indices["dim"], Cr=Cr, F=F) #inicializando evolucion diferencial
+#========================= Fin inicializacion
 
-padres = evolucion_diferencial.inicializacion(LI=Li,LS=Ls)
+#========================= KFolds =========================
+kfold = StratifiedKFold(n_splits=folders)
 
-#obteniendo 1er error de los primeros padres antes de entrar al algoritmo de evolucion diferencial
+for train_index, test_index in kfold.split(X,y):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y_bin[train_index], y_bin[test_index]
+#========================= Fin KFolds
+
+#========================= Algoritmo Evolución Diferencial =========================
+padres = evolucion_diferencial.inicializacion(LI=Li, LS=Ls) #generando la poblacion inicial
 error_en_padres = np.zeros((Np,1))
-for i in range(Np):
-    error_padres = evolucion_diferencial.fitness_error_mlp(MLP_object=perceptron,X=X, y=y_bin, weights=padres[i].reshape(1,-1))
-    error_en_padres[i] = error_padres
-
 error_en_crossover = np.zeros((Np,1))
 
-aux = 0
+for i in range(Np):
+    error_padres = evolucion_diferencial.fitness_error_mlp(MLP_object=perceptron,X=X_train, y=y_train, weights=padres[i].reshape(1,-1))
+    error_en_padres[i] = error_padres
+
 while(aux <= generaciones):
+    #print('==== generacion:',aux,' =====')
     mutado = evolucion_diferencial.mutacion(padres)
     crossover = evolucion_diferencial.recombinacion(padres,mutado)
 
     for i in range(crossover.shape[0]):
-        error_crossover = evolucion_diferencial.fitness_error_mlp(MLP_object=perceptron,X=X, y=y_bin, weights=crossover[i].reshape(1,-1))
+        error_crossover = evolucion_diferencial.fitness_error_mlp(MLP_object=perceptron,X=X_train, y=y_train, weights=crossover[i].reshape(1,-1))
         error_en_crossover[i] = error_crossover
 
     padres,error_en_padres = evolucion_diferencial.seleccion(padres,error_en_padres,crossover,error_en_crossover)
-    #for j in range(Np):
+
     aux += 1
+#========================= Fin Algoritmo Evolución Diferencial
 
-prediction = perceptron.train_model(X_train=X, y_train=y_bin, DE_population=padres[error_en_padres.argmin()].reshape(1, -1))
-print(accuracy_score(y, decode_onehot(prediction)))
-
+#========================= data de prueba con la ultima generacion DE =========================
+for i in range(Np):
+    prediction = perceptron.train_model(X_train=X_train, y_train=y_train, DE_population=padres[i].reshape(1, -1))
+    print(accuracy_score(decode_onehot(y_train), decode_onehot(prediction)))
